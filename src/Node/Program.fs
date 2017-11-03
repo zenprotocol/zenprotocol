0d6aee112092166899a8b5c8c6e5d314b08b0023
Doron Somech <somdoron@gmail.com>
2017-11-03 14:33:34 +0200

    add config and command line args to zen-node

--- src/Node/Program.fs
+++ src/Node/Program.fs
@@ -1,5 +1,20 @@
 ﻿open FsNetMQ
+open FSharp.Configuration
+open Argu
 open Infrastructure
+
+[<NoAppSettings>]
+type Argument = 
+    | Chain of string
+    | Localhost    
+    with
+        interface IArgParserTemplate with
+            member s.Usage =
+                match s with
+                | Chain _ -> "specify chain (local,test or main)."
+                | Localhost -> "specify if the node should local chain host"                              
+
+type Config = YamlConfig<"config.yaml">
 
 let busName = "main"
 
@@ -15,14 +30,41 @@ let createBroker () =
         Poller.run poller           
 )
 
+let getNetworkParameters (config:Config) = 
+    match config.chain with
+    | "local" -> config.networks.local.listen,config.networks.local.bind,config.networks.local.seeds
+    | "test" -> config.networks.test.listen,config.networks.test.bind,config.networks.test.seeds
+    | "main" -> config.networks.main.listen,config.networks.main.bind,config.networks.main.seeds
+    | c -> failwithf "unkown chain %s" c 
+
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    let errorHandler = ProcessExiter(colorizer = function ErrorCode.HelpText -> None | _ -> Some System.ConsoleColor.Red)
+
+    let config = new Config()           
+    config.Load("config.yaml")
     
+    let parser = ArgumentParser.Create<Argument>(programName = "zen-node.exe", errorHandler = errorHandler)            
+    let results = parser.Parse argv
+    
+    List.iter (fun arg -> 
+        match arg with 
+        | Chain chain -> config.chain <- chain            
+        | Localhost ->
+            config.chain <- "local"
+            config.networks.local.listen <- true
+            config.networks.local.seeds.Clear ()              
+    ) (results.GetAllResults())
+                              
     use brokerActor = createBroker ()    
     use blockchainActor = Blockchain.Main.main busName
     
+    let listen,bind,seeds = getNetworkParameters config
+    use networkActor = Network.Main.main busName config.externalIp listen bind seeds
+            
     printfn "running..."
+    
+    printfn "Press enter to exit"
     
     System.Console.ReadLine () |> ignore
     
