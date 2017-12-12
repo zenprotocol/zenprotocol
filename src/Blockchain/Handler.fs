676cb01049b8652b6efa3334ad673d1b37a6c137
Doron Somech <somdoron@gmail.com>
2017-12-12 15:45:10 +0200

    handling orphan transactions

--- src/Blockchain/Handler.fs
+++ src/Blockchain/Handler.fs
@@ -8,37 +8,13 @@ open Consensus
 open Blockchain
 open Blockchain.EffectsWriter
 
-let handleCommand command (utxoSet, mempool) =
-    effectsWriter 
-        {
-            match command with
-            | ValidateTransaction tx ->
-                let txHash = Transaction.hash tx
-            
-                match MemPool.containsTransaction txHash mempool with
-                | true -> return (utxoSet,mempool) // Nothing to do, already in mempool
-                | false -> 
-                    match Transaction.validateInputs utxoSet tx with
-                    | Ok tx ->
-                                                                        
-                        let utxoSet = UtxoSet.handleTransaction txHash tx utxoSet
-                        let mempool = MemPool.add txHash tx mempool
-                        
-                        do! publish (TransactionAddedToMemPool (txHash,tx))
-                        
-                        Log.info "Transaction %s added to mempool" (Hash.toString txHash)
-                        
-                        return (utxoSet,mempool)
-                    | Error error -> 
-                        // TODO: we should do something with the error here, like writing to log
-                        // and banning peer?
-                        // TODO: check if should be added to orphan list
-                        return (utxoSet,mempool)                                             
-        }
-        
-let handleRequest request reply (utxoSet, mempool) = 
-    ret (utxoSet,mempool)
+let handleCommand command (utxoSet, mempool, orphanPool) =   
+    match command with
+    | ValidateTransaction tx -> TransactionHandler.validateTransaction tx (utxoSet, mempool, orphanPool)
+                                                     
+let handleRequest request reply (utxoSet, mempool, orphanPool) = 
+    ret (utxoSet,mempool, orphanPool)
     
-let handleEvent event (utxoSet, mempool) = 
-    ret (utxoSet,mempool)
+let handleEvent event (utxoSet, mempool, orphanPool) = 
+    ret (utxoSet,mempool, orphanPool)
     
---
+++ src/Blockchain/TransactionHandler.fs
@@ -0,0 +1,66 @@
+module Blockchain.TransactionHandler
+
+open Blockchain.EffectsWriter
+open Messaging.Events
+open Infrastructure
+open Infrastructure.Writer
+open Consensus
+open Consensus.Transaction
+
+let validateOrphanTransaction (utxoSet, mempool, orphanPool) txHash tx  =                                 
+    effectsWriter {            
+        match Transaction.validateInputs utxoSet tx with
+        | Ok tx ->
+            let utxoSet = UtxoSet.handleTransaction txHash tx utxoSet
+            let mempool = MemPool.add txHash tx mempool
+            
+            do! publish (TransactionAddedToMemPool (txHash,tx))
+            Log.info "Orphan transaction %s added to mempool" (Hash.toString txHash)
+            
+            let orphanPool = OrphanPool.remove txHash orphanPool
+            return utxoSet, mempool, orphanPool
+        | Error (Orphan _) ->
+            // transacation is still orphan, nothing to do
+            return utxoSet, mempool, orphanPool            
+        | Error error ->
+            Log.info "Orphan transacation %s failed validation: %A" (Hash.toString txHash) error
+        
+            let orphanPool = OrphanPool.remove txHash orphanPool
+            return utxoSet, mempool, orphanPool
+                     
+    }
+        
+let validateTransaction tx (utxoSet, mempool, orphanPool) =
+    effectsWriter {
+        let txHash = Transaction.hash tx
+                            
+        if MemPool.containsTransaction txHash mempool || OrphanPool.containsTransaction txHash orphanPool then                
+            return (utxoSet,mempool, orphanPool) // Nothing to do, already in mempool
+        else 
+            match Transaction.validateBasic tx with
+            | Error error ->                        
+                Log.info "Transacation %s failed basic validation: %A" (Hash.toString txHash) error
+                
+                return (utxoSet,mempool, orphanPool)
+            | Ok tx ->
+                match Transaction.validateInputs utxoSet tx with
+                | Error (General error) ->
+                    Log.info "Transacation %s failed inputs validation: %s" (Hash.toString txHash) error
+                            
+                    return (utxoSet,mempool, orphanPool)
+                | Error (Orphan tx) ->                                                                                                         
+                    let orphanPool = OrphanPool.add txHash tx orphanPool
+                    
+                    Log.info "Transaction %s is orphan, adding to orphan pool" (Hash.toString txHash)                                                
+                                        
+                    return (utxoSet,mempool, orphanPool)                                
+                | Ok tx ->                                                                        
+                    let utxoSet = UtxoSet.handleTransaction txHash tx utxoSet
+                    let mempool = MemPool.add txHash tx mempool
+                    
+                    do! publish (TransactionAddedToMemPool (txHash,tx))
+                    
+                    Log.info "Transaction %s added to mempool" (Hash.toString txHash)
+                    
+                    return! OrphanPool.foldWriter validateOrphanTransaction (utxoSet, mempool, orphanPool) orphanPool
+    }
