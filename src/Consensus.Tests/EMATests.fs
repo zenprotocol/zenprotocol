a9d0a93b6f7a0309eb50205aafecf7ae36a5b8d5
Nathan Cook <nathan.cook@gmail.com>
2018-01-15 19:11:01 +0200

    Test EMA and set block intervals in milliseconds

--- src/Consensus.Tests/EMATests.fs
+++ src/Consensus.Tests/EMATests.fs
@@ -19,22 +19,53 @@ let exponentialRandomGen (lambda:float) =
     uniformRandomGen
         |> Gen.map (fun f -> -1. * lambda * System.Math.Log(1.-f))
 
-let seqGen n (itemGen:Gen<'a>) =
-    Gen.sequenceToSeq <| Seq.init n (fun _ -> itemGen)
-
-let sumSeqGen (seqGen:Gen<seq<float>>) =
-    Gen.map (Seq.scan (+) 0.) seqGen |> Gen.map (Seq.skip 1)
+let sumListGen (lGen:Gen<list<float>>) =
+    Gen.map (List.scan (+) 0. >> List.tail) lGen
 
 let exponentialSumGen n (lambda:float) =
-    exponentialRandomGen lambda |> seqGen n |> sumSeqGen
+    Gen.listOfLength n (exponentialRandomGen lambda) |> sumListGen
 
-let timestampGen = Gen.map (Seq.map (fun f -> (uint64 (f * 1000.)))) (exponentialSumGen 500 240.)
+let timestampGen blocktime =
+    Gen.sized <| 
+        fun s -> 
+            Gen.map (List.map (fun f -> (uint64 (f * 1000.))))
+                    (exponentialSumGen s blocktime)
 
-type TimestampSequence =
-    static member TimestampArb() = Arb.fromGen timestampGen
+type NormalIntervalTimestamps =
+    static member TimestampArb() = Arb.fromGen (timestampGen 240.)
 
-[<Property(Arbitrary = [|typeof<TimestampSequence>|])>]
-let ``Default difficulty for first 11 blocks``(tstamps:seq<uint64>) =
+[<Property(Arbitrary = [|typeof<NormalIntervalTimestamps>|], StartSize=50)>]
+let ``Default difficulty for first 11 blocks``(tstamps:uint64 list) =
     let ema = create ChainParameters.Main
-    let emas = Seq.scan (fun e ts -> add ChainParameters.Main ts e) ema (Seq.take 11 tstamps)
-    Seq.forall (fun e -> e.difficulty = ema.difficulty) emas
+    let emas = List.scan (fun e ts -> add ChainParameters.Main ts e) ema (List.take 11 tstamps)
+    List.forall (fun e -> e.difficulty = ema.difficulty) emas
+
+type FastIntervalTimestamps =
+    static member TimestampArb() = Arb.fromGen (timestampGen 180.)
+
+[<Property(Arbitrary = [|typeof<FastIntervalTimestamps>|], StartSize=1000)>]
+let ``Difficulty increases when blocks are generated quickly``(tstamps:uint64 list) =
+    let ema = create ChainParameters.Main
+    let emas = List.scan (fun e ts -> add ChainParameters.Main ts e) ema tstamps
+    let initTarget = Hash.toBigInt (ChainParameters.proofOfWorkLimit ChainParameters.Main)
+    let finalTarget =
+        List.last emas
+        |> fun e -> e.difficulty
+        |> Difficulty.uncompress
+        |> Hash.toBigInt
+    finalTarget < initTarget
+
+type SlowIntervalTimestamps =
+    static member TimestampArb() = Arb.fromGen (timestampGen 300.)
+
+[<Property(Arbitrary = [|typeof<SlowIntervalTimestamps>|], StartSize=1000)>]
+let ``Difficulty is always more than the chain minimum``(tstamps:uint64 list) =
+    let ema = create ChainParameters.Main
+    let emas = List.scan (fun e ts -> add ChainParameters.Main ts e) ema tstamps
+    let targetLimit = Hash.toBigInt (ChainParameters.proofOfWorkLimit ChainParameters.Main)
+    List.forall
+        (fun e -> 
+            e.difficulty
+            |> Difficulty.uncompress
+            |> Hash.toBigInt <= targetLimit)
+        emas
