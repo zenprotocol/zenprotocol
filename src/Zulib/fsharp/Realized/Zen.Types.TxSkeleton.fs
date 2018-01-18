f585f62bcaee2da393bd169e2c5615309e666b11
Ash <ash@ash-ubuntu16.04>
2018-01-18 14:48:01 +0200

    nits

--- src/Zulib/fsharp/Realized/Zen.Types.TxSkeleton.fs
+++ src/Zulib/fsharp/Realized/Zen.Types.TxSkeleton.fs
@@ -9,50 +9,49 @@ module V = Zen.Vector
 module U64 = FStar.UInt64
 
 type txSkeleton =
-    { // A map from asset identifiers to the total amount of that asset and the corresponding pointedOutputs
-      inputs : Collections.Map<asset, (U64.t * list<pointedOutput>)>;
-      // A map from asset identifiers to the total amount of that asset and the corresponding outputs
-      outputs : Collections.Map<asset,(U64.t * list<output>)> }
+    { inputs : Collections.Map<asset, U64.t * list<pointedOutput>>;
+      outputs : Collections.Map<asset, U64.t * list<output>> }
 
 let getAvailableTokens (asset : asset) (txSkeleton : txSkeleton) : Cost.t<uint64, unit> =
     lazy (let unlockedAmount =
               match Map.tryFind asset txSkeleton.inputs with
               | None -> 0UL
-              | Some (amount, _) -> amount
+              | Some(amount, _) -> amount
 
           let lockedAmount =
               match Map.tryFind asset txSkeleton.outputs with
               | None -> 0UL
-              | Some (amount, _) -> amount
+              | Some(amount, _) -> amount
 
           unlockedAmount - lockedAmount)
     |> Cost.C
 
-let insertPointedOutput (pointedOutput : pointedOutput) (txSkeleton : txSkeleton) : txSkeleton =
-    let {asset=asset; amount=amount} = (snd pointedOutput).spend
+let insertPointedOutput (pointedOutput : pointedOutput)
+    (txSkeleton : txSkeleton) : txSkeleton =
+    let { asset = asset; amount = amount } = (snd pointedOutput).spend
+
     let inputs =
-        if Map.containsKey asset txSkeleton.inputs
-        then
+        if Map.containsKey asset txSkeleton.inputs then
             let oldAmount, pointedOutputs = Map.find asset txSkeleton.inputs
-            txSkeleton.inputs |> Map.add asset (amount + oldAmount, pointedOutput::pointedOutputs)
-        else
-            txSkeleton.inputs |> Map.add asset (amount, [pointedOutput])
-    {txSkeleton with inputs=inputs}
+            txSkeleton.inputs
+            |> Map.add asset
+                   (amount + oldAmount, pointedOutput :: pointedOutputs)
+        else txSkeleton.inputs |> Map.add asset (amount, [ pointedOutput ])
+    { txSkeleton with inputs = inputs }
 
 let insertOutput (output : output) (txSkeleton : txSkeleton) : txSkeleton =
-    let {asset=asset; amount=amount} = output.spend
+    let { asset = asset; amount = amount } = output.spend
+
     let outputs =
-        if Map.containsKey asset txSkeleton.inputs
-        then
+        if Map.containsKey asset txSkeleton.inputs then
             let oldAmount, outputs = Map.find asset txSkeleton.outputs
-            txSkeleton.outputs |> Map.add asset (amount + oldAmount, output::outputs)
-        else
-            txSkeleton.outputs |> Map.add asset (amount, [output])
-    {txSkeleton with outputs=outputs}
+            txSkeleton.outputs
+            |> Map.add asset (amount + oldAmount, output :: outputs)
+        else txSkeleton.outputs |> Map.add asset (amount, [ output ])
+    { txSkeleton with outputs = outputs }
 
 let addInput (pointedOutput : pointedOutput) (txSkeleton : txSkeleton) : Cost.t<txSkeleton, unit> =
-    lazy (insertPointedOutput pointedOutput txSkeleton )
-    |> Cost.C
+    lazy (insertPointedOutput pointedOutput txSkeleton) |> Cost.C
 
 let rec addInputs (_ : Prims.nat) (pointedOutputs : V.t<pointedOutput, unit>)
         (txSkeleton : txSkeleton) : Cost.t<txSkeleton, unit> =
@@ -65,8 +64,7 @@ let rec addInputs (_ : Prims.nat) (pointedOutputs : V.t<pointedOutput, unit>)
     |> Cost.C
 
 let addOutput (output : output) (txSkeleton : txSkeleton) : Cost.t<txSkeleton, unit> =
-    lazy (insertOutput output txSkeleton)
-    |> Cost.C
+    lazy (insertOutput output txSkeleton) |> Cost.C
 
 let lockToContract (spend : spend) (contractHash : hash)
     (txSkeleton : txSkeleton) : Cost.t<txSkeleton, unit> =
@@ -75,8 +73,7 @@ let lockToContract (spend : spend) (contractHash : hash)
           let output =
               { lock = lock;
                 spend = spend }
-
-          insertOutput output txSkeleton )
+          insertOutput output txSkeleton)
     |> Cost.C
 
 let lockToPubKey (spend : spend) (pkHash : hash) (txSkeleton : txSkeleton) : Cost.t<txSkeleton, unit> =
@@ -85,8 +82,7 @@ let lockToPubKey (spend : spend) (pkHash : hash) (txSkeleton : txSkeleton) : Cos
           let output =
               { lock = lock;
                 spend = spend }
-
-          insertOutput output txSkeleton )
+          insertOutput output txSkeleton)
     |> Cost.C
 
 let lockToAddress (spend : spend) (address : hash) (txSkeleton : txSkeleton) : Cost.t<txSkeleton, unit> =
@@ -103,25 +99,33 @@ let addChangeOutput (asset : asset) (contractHash : contractHash)
           lockToContract spend contractHash txSkeleton |> Cost.__force)
     |> Cost.C
 
-let mint (amount:U64.t) (contractHash:contractHash) (txSkeleton : txSkeleton)
-    : Cost.t<txSkeleton, unit> =
-    lazy (let mintSpend = {asset=contractHash; amount=amount}
+let mint (amount : U64.t) (contractHash : contractHash)
+    (txSkeleton : txSkeleton) : Cost.t<txSkeleton, unit> =
+    lazy (let mintSpend =
+              { asset = contractHash;
+                amount = amount }
+
           let mintLock = ContractLock(contractHash, 0I, Empty)
-          let mintOutput = {lock=mintLock; spend=mintSpend}
+
+          let mintOutput =
+              { lock = mintLock;
+                spend = mintSpend }
+
           let mintTxHash = Array.create 32 0uy
-          let mintOutpoint = {txHash=mintTxHash; index=0u}
+
+          let mintOutpoint =
+              { txHash = mintTxHash;
+                index = 0u }
+
           let pointedOutput = mintOutpoint, mintOutput
-          addInput pointedOutput txSkeleton
-          |> Cost.__force )
+          addInput pointedOutput txSkeleton |> Cost.__force)
     |> Cost.C
 
-let isValid (txSkeleton : txSkeleton) : Cost.t<bool, unit> =
-    lazy ( if keySet txSkeleton.inputs <> keySet txSkeleton.outputs
-           then false
-           else
-                txSkeleton.inputs
-                |> Map.forall(fun asset (inputAmount, _) ->
-                    let outputAmount, _ = Map.find asset txSkeleton.outputs
-                    inputAmount = outputAmount)
-         )
+let isValid(txSkeleton : txSkeleton) : Cost.t<bool, unit> =
+    lazy (if keySet txSkeleton.inputs <> keySet txSkeleton.outputs then false
+          else
+              txSkeleton.inputs |> Map.forall(fun asset (inputAmount, _) ->
+                                       let outputAmount, _ =
+                                           Map.find asset txSkeleton.outputs
+                                       inputAmount = outputAmount))
     |> Cost.C
