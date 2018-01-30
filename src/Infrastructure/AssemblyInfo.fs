f6ce36ba3bb11b5cd57e5469d1ab4ad49b0ddbb3
A L Manning <asher@zenprotocol.com>
2018-01-30 14:54:40 +0200

    update myget version

--- src/Infrastructure/AssemblyInfo.fs
+++ src/Infrastructure/AssemblyInfo.fs
@@ -4,7 +4,7 @@ open System.Reflection
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [<assembly: AssemblyTitle("Infrastructure")>]
@@ -16,8 +16,8 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [<assembly: ComVisible(false)>]
 
@@ -25,17 +25,17 @@ open System.Runtime.InteropServices
 [<assembly: Guid("81161F64-D019-422C-A3CF-321CDE6CA209")>]
 
 // Version information for an assembly consists of the following four values:
-// 
+//
 //       Major Version
-//       Minor Version 
+//       Minor Version
 //       Build Number
 //       Revision
-// 
-// You can specify all the values or you can default the Build and Revision Numbers 
+//
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("0.0.1.0")>]
-[<assembly: AssemblyFileVersion("0.0.1.0")>]
+[<assembly: AssemblyVersion("0.0.1.1")>]
+[<assembly: AssemblyFileVersion("0.0.1.1")>]
 
 do
     ()
