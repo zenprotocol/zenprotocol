6ec21b494d9bc178362e5a145b28946ca2a487a4
A L Manning <asher@zenprotocol.com>
2018-02-11 13:26:41 +0200

    merge attempt 4

--- src/Blockchain.Tests/MintingContractTests.fs
+++ src/Blockchain.Tests/MintingContractTests.fs
@@ -70,67 +70,6 @@ let clean() =
 [<OneTimeSetUp>]
 let setUp = fun () ->
     clean()
-<<<<<<< HEAD
-    try 
-        activateContract """
-        open Zen.Types
-        open Zen.Vector
-        open Zen.Util
-        open Zen.Base
-        open Zen.Cost
-        open Zen.Assets
-                
-        module ET = Zen.ErrorT
-        module Tx = Zen.TxSkeleton
-                
-        val cf: txSkeleton -> string -> option lock -> #l:nat -> wallet l -> cost nat 15
-        let cf _ _ _ #l _ = ret (64 + (64 + 64 + (l * 128 + 192)) + 19 + 22)
-        
-        let buy txSkeleton contractHash returnAddress = 
-          let! tokens = Tx.getAvailableTokens zenAsset txSkeleton in
-                
-          let txSkeleton = 
-            Tx.lockToContract zenAsset tokens contractHash txSkeleton
-            >>= Tx.mint tokens contractHash
-            >>= Tx.lockToAddress contractHash tokens returnAddress in
-                 
-          ET.retT txSkeleton	
-            
-        let redeem #l txSkeleton contractHash returnAddress (wallet:wallet l) = 
-          let! tokens = Tx.getAvailableTokens contractHash txSkeleton in
-        
-          let txSkeleton = 
-            Tx.destroy tokens contractHash txSkeleton
-            >>= Tx.lockToAddress zenAsset tokens returnAddress
-            >>= Tx.fromWallet zenAsset tokens contractHash wallet in
-        
-          ET.of_optionT "contract doesn't have enough zens to pay you" txSkeleton
-        
-        val main: txSkeleton -> hash -> string -> option lock -> #l:nat -> wallet l -> cost (result txSkeleton) (64 + (64 + 64 + (l * 128 + 192)) + 19 + 22)
-        let main txSkeleton contractHash command returnAddress #l wallet =
-          match returnAddress with
-          | Some returnAddress -> 
-              if command = "redeem" then
-                redeem txSkeleton contractHash returnAddress wallet
-              else if command = "" || command = "buy" then
-                buy txSkeleton contractHash returnAddress
-                |> autoInc    
-              else 
-                ET.autoFailw "unsupported command"
-          | None ->
-              ET.autoFailw "returnAddress is required"
-        """ account session state
-        |> function
-        | Ok (state', cHash') -> 
-            cHash <- cHash'
-            state <- state'
-        | Error error ->
-            failwith error
-    with _ as ex ->
-        printfn "Ex: %A" ex
-        
-        
-=======
     activateContract """
     open Zen.Types
     open Zen.Vector
@@ -176,13 +115,13 @@ let setUp = fun () ->
     val main: txSkeleton -> hash -> string -> option lock -> #l:nat -> wallet l -> cost (result (txSkeleton ** option message)) (M.(64 + (64 + 64 + (l * 128 + 192) + 0) + 26 + 22) <: nat)
     let main txSkeleton contractHash command returnAddress #l wallet =
       match returnAddress with
-      | Some returnAddress -> 
+      | Some returnAddress ->
         if command = "redeem" then
           redeem txSkeleton contractHash returnAddress wallet
         else if command = "" || command = "buy" then
           buy txSkeleton contractHash returnAddress
-          |> autoInc    
-        else 
+          |> autoInc
+        else
           ET.autoFailw "unsupported command"
       | None ->
         ET.autoFailw "returnAddress is required"
@@ -194,7 +133,6 @@ let setUp = fun () ->
     | Error error ->
         failwith error
 
->>>>>>> f556134882675bd6bf55aac022e2375191ec8f31
 [<TearDown>]
 let tearDown = fun () ->
     clean()
