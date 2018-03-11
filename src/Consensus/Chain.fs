ffa0b3c11531f881330e370626a66812aed62dd3
Doron Somech <somdoron@gmail.com>
2018-03-11 12:55:53 +0200

    change testnet smoothing factor

--- src/Consensus/Chain.fs
+++ src/Consensus/Chain.fs
@@ -32,10 +32,10 @@ let mainParameters =
 let testParameters =
     {   proofOfWorkLimit=Difficulty.uncompress 0x20fffffful;
         blockInterval=60UL*1000UL;
-        smoothingFactor=0.05;
+        smoothingFactor=0.0055;
         maxBlockWeight=1000_000_000I;
         sacrificePerByteBlock=1UL;
-        genesisHash= get <| Hash.fromString "9c38be3ee5e1a3d6e3c4f7184ff1b1cc99b44dfa12ce2cfc8ba437eeaa33627a";
+        genesisHash= get <| Hash.fromString "ff8a3da32377e3684c54c75b22c3bee443b0a055a763361877c3646a41b981a2";
         genesisTime=1517828985040UL
     }
 
@@ -67,7 +67,7 @@ let getGenesisHash =
     function
     | Main -> Hash.zero
     | Test ->
-        Hash.fromString "9c38be3ee5e1a3d6e3c4f7184ff1b1cc99b44dfa12ce2cfc8ba437eeaa33627a" |>
+        Hash.fromString "ff8a3da32377e3684c54c75b22c3bee443b0a055a763361877c3646a41b981a2" |>
         function | Ok value -> value | Error error -> failwith error
     | Local ->
         Hash.fromString "7ffa8c6b1525b8b98ba7847a524a0383659d111d793d5249f4b39b0c84d06b4c" |>
