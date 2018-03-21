0491de875db51d728b58a79ba9140776d248c17d
Sharon Urban <urban_sharon@hotmail.com>
2018-03-21 21:12:01 +0200

    zulib-data serialization

--- src/Consensus/Serialization.fs
+++ src/Consensus/Serialization.fs
@@ -1,13 +1,14 @@
 module Consensus.Serialization
-open Consensus
 
-open Consensus
 open System
 open Crypto
 open Types
+open Zen.Types.Data
 open FsNetMQ
 open FsNetMQ.Stream
 open FsNetMQ.Stream.Reader
+
+#nowarn "40"   // Ignore recurssive objects warnings
 
 [<Literal>]
 let SerializedHeaderSize = 100 
@@ -83,8 +84,17 @@ module private Serialization =
         }
     
     module List =
-        let read readerFn = reader {
-            let! length = readNumber4
+        let writeBody ops writerFn list =
+            let write list writerFn stream = //TODO: use fold?
+                let mutable stream = stream //TODO: this is not 'stream' if ops are Counters
+                for item in list do
+                    stream <- writerFn ops item stream
+                stream
+            write list writerFn
+        let write ops writerFn list =
+            ops.writeNumber4 (List.length list |> uint32)
+            >> writeBody ops writerFn list
+        let readBody readerFn length = reader {
             let! list = reader {
                 for _ in [1..int length] do
                     let! item = readerFn
@@ -92,14 +102,11 @@ module private Serialization =
             }
             return List.ofSeq list
         }
-        let write ops writerFn list =
-            let write list writerFn stream = //TODO: use fold?
-                let mutable stream = stream //TODO: this is not 'stream' if ops are Counters
-                for item in list do
-                    stream <- writerFn ops item stream
-                stream
-            ops.writeNumber4 (List.length list |> uint32)
-            >> write list writerFn
+        let read readerFn = reader {
+            let! length = readNumber4
+            let! list = readBody readerFn length
+            return List.ofSeq list
+        }
     
     module Asset = 
         let write ops = fun (cHash, token) ->
@@ -149,65 +156,7 @@ module private Serialization =
             | _ ->
                 yield! fail
         }
-    
-    module Witness =
-        [<Literal>]
-        let private SerializedPKWitness = 1uy
-        [<Literal>]
-        let private SerializedContractWitness = 2uy
-    
-        let write ops = function
-            | PKWitness (bytes, Signature signature) ->
-                ops.writeByte SerializedPKWitness
-                >> ops.writeBytes bytes Crypto.SerializedPublicKeyLength
-                >> ops.writeBytes signature Crypto.SerializedSignatureLength
-            | ContractWitness cw ->
-                let (Data data) = cw.data
-                let dataLength = Array.length data
-                ops.writeByte SerializedContractWitness
-                >> ops.writeHash cw.cHash
-                >> ops.writeString cw.command
-                >> ops.writeNumber4 (uint32 dataLength)
-                >> ops.writeBytes data dataLength
-                >> Option.write ops ops.writeNumber4 cw.returnAddressIndex
-                >> ops.writeNumber4 cw.beginInputs
-                >> ops.writeNumber4 cw.beginOutputs
-                >> ops.writeNumber4 cw.inputsLength
-                >> ops.writeNumber4 cw.outputsLength
-                >> ops.writeNumber4 cw.cost
-        let read = reader {
-            let! discriminator = Byte.read
-            match discriminator with
-            | SerializedPKWitness ->
-                let! publicKey = readBytes Crypto.SerializedPublicKeyLength
-                let! signature = readBytes Crypto.SerializedSignatureLength
-                return PKWitness (publicKey, Signature signature)
-            | SerializedContractWitness ->
-                let! cHash = Hash.read
-                let! command = readString
-                let! dataLength = readNumber4
-                let! data = readBytes (int dataLength)
-                let! returnAddressIndex = Option.read readNumber4
-                let! beginInputs = readNumber4
-                let! beginOutputs = readNumber4
-                let! inputsLength = readNumber4
-                let! outputsLength = readNumber4
-                let! cost = readNumber4
-                return ContractWitness {
-                    cHash = Hash.Hash cHash
-                    command = command
-                    data = Data data
-                    returnAddressIndex = returnAddressIndex
-                    beginInputs = beginInputs
-                    beginOutputs = beginOutputs
-                    inputsLength = inputsLength
-                    outputsLength = outputsLength
-                    cost = cost
-                }
-            | _ ->
-                yield! fail
-        }
-    
+
     module Lock =
         [<Literal>]
         let private SerializedPK = 1uy
@@ -258,6 +207,244 @@ module private Serialization =
                 return Lock.ActivationSacrifice
             | SerializedDestroy ->
                 return Lock.Destroy
+            | _ ->
+                yield! fail
+        }
+
+    module Data =
+        [<Literal>]
+        let private I64Data = 1uy
+        [<Literal>]
+        let private I64ArrayData = 2uy
+        [<Literal>]
+        let private ByteData = 3uy
+        [<Literal>]
+        let private ByteArrayData = 4uy
+        [<Literal>]
+        let private U32Data = 5uy
+        [<Literal>]
+        let private U32ArrayData = 6uy
+        [<Literal>]
+        let private U64Data = 7uy
+        [<Literal>]
+        let private U64ArrayData = 8uy
+        [<Literal>]
+        let private StringData = 9uy
+        [<Literal>]
+        let private StringArrayData = 10uy
+        [<Literal>]
+        let private HashData = 11uy
+        [<Literal>]
+        let private HashArrayData = 12uy
+        [<Literal>]
+        let private LockData = 13uy
+        [<Literal>]
+        let private LockArrayData = 14uy
+        [<Literal>]
+        let private TupleData = 15uy
+        [<Literal>]
+        let private DictData = 16uy
+        [<Literal>]
+        let private EmptyData = 17uy
+
+        module Int64 = 
+            let write ops = uint64 >> ops.writeNumber8 
+            let read = reader {
+                let! i = readNumber8
+                return int64 i
+            }
+            
+        module Array =
+            let write obs fn = Array.toList >> List.write obs fn
+            let readDtuple readerFn = reader {
+                let! seq = List.read readerFn
+                return Prims.Mkdtuple2 (int64 (Seq.length seq), Array.ofSeq seq)
+            }
+        
+        module Hash = 
+            let write ops = Consensus.Hash.Hash >> ops.writeHash
+
+        module Lock = 
+            let write ops = ZFStar.fstToFsLock >> Lock.write ops
+            let read = reader {
+                let! lock = Lock.read
+                return ZFStar.fsToFstLock lock
+            }
+
+        module String = 
+            let write ops = ZFStar.fstToFsString >> ops.writeString 
+            let read = reader {
+                let! s = readString
+                return ZFStar.fsToFstString s
+            }
+            
+        let rec write ops = function
+            | I64 i ->
+                ops.writeByte I64Data
+                >> Int64.write ops i
+            | I64Array (Prims.Mkdtuple2 (_, arr)) ->
+                ops.writeByte I64ArrayData
+                >> Array.write ops Int64.write arr
+            | Byte b ->
+                ops.writeByte ByteData
+                >> ops.writeByte b
+            | ByteArray (Prims.Mkdtuple2 (_, arr)) ->
+                ops.writeByte ByteArrayData
+                >> Array.write ops (fun ops -> ops.writeByte) arr
+            | U32 i ->
+                ops.writeByte U32Data
+                >> ops.writeNumber4 i
+            | U32Array (Prims.Mkdtuple2 (_, arr)) ->
+                ops.writeByte U32ArrayData
+                >> Array.write ops (fun ops -> ops.writeNumber4) arr
+            | U64 i ->
+                ops.writeByte U64Data
+                >> ops.writeNumber8 i
+            | U64Array (Prims.Mkdtuple2 (_, arr)) ->
+                ops.writeByte U64ArrayData
+                >> Array.write ops (fun ops -> ops.writeNumber8) arr
+            | String s ->
+                ops.writeByte StringData
+                >> String.write ops s
+            | StringArray (Prims.Mkdtuple2 (_, arr)) ->
+                ops.writeByte StringArrayData
+                >> Array.write ops String.write arr
+            | Hash hash ->
+                ops.writeByte HashData
+                >> Hash.write ops hash
+            | HashArray (Prims.Mkdtuple2 (_, arr)) ->
+                ops.writeByte HashArrayData
+                >> Array.write ops Hash.write arr
+            | Lock l ->
+                ops.writeByte LockData
+                >> Lock.write ops l
+            | LockArray (Prims.Mkdtuple2 (_, arr)) ->
+                ops.writeByte LockArrayData
+                >> Array.write ops Lock.write arr
+            | Tuple (a, b) ->
+                ops.writeByte TupleData
+                >> write ops a
+                >> write ops b
+            | Dict (DataDict (map, len)) ->
+                let entries = Map.toList map
+                ops.writeByte DictData
+                >> ops.writeNumber4 len
+                >> List.writeBody ops String.write (entries |> List.map fst)
+                >> List.writeBody ops write (entries |> List.map snd)
+            | Empty ->
+                ops.writeByte EmptyData
+
+        let rec read = reader {
+            let! discriminator = Byte.read
+            match discriminator with
+            | I64Data ->
+                let! i = Int64.read
+                return I64 i
+            | I64ArrayData ->
+                let! arr = Array.readDtuple Int64.read
+                return I64Array arr
+            | ByteData ->
+                let! byte = Byte.read
+                return Byte byte
+            | ByteArrayData ->
+                let! arr = Array.readDtuple Byte.read
+                return ByteArray arr
+            | U32Data ->
+                let! i = readNumber4
+                return U32 i
+            | U32ArrayData ->
+                let! arr = Array.readDtuple readNumber4
+                return U32Array arr
+            | U64Data ->
+                let! i = readNumber8
+                return U64 i
+            | U64ArrayData ->
+                let! arr = Array.readDtuple readNumber8
+                return U64Array arr
+            | StringData ->
+                let! s = String.read
+                return String s
+            | StringArrayData ->
+                let! arr = Array.readDtuple String.read
+                return StringArray arr
+            | HashData ->
+                let! hash = Hash.read
+                return Hash hash
+            | HashArrayData ->
+                let! arr = Array.readDtuple Hash.read
+                return HashArray arr
+            | LockData ->
+                let! lock = Lock.read
+                return Lock lock
+            | LockArrayData ->
+                let! arr = Array.readDtuple Lock.read
+                return LockArray arr
+            | TupleData ->
+                let! a = read
+                let! b = read
+                return Tuple (a,b)
+            | DictData ->
+                let! len = readNumber4
+                let! keys = List.readBody String.read len
+                let! values = List.readBody read len
+                let map = List.zip keys values |> Map.ofSeq
+                return Dict (DataDict (map, len))
+            | EmptyData ->
+                return Empty
+            | _ ->
+                yield! fail
+        }
+
+    module Witness =
+        [<Literal>]
+        let private SerializedPKWitness = 1uy
+        [<Literal>]
+        let private SerializedContractWitness = 2uy
+    
+        let write ops = function
+            | PKWitness (bytes, Signature signature) ->
+                ops.writeByte SerializedPKWitness
+                >> ops.writeBytes bytes Crypto.SerializedPublicKeyLength
+                >> ops.writeBytes signature Crypto.SerializedSignatureLength
+            | ContractWitness cw ->
+                ops.writeByte SerializedContractWitness
+                >> ops.writeHash cw.cHash
+                >> ops.writeString cw.command
+                >> Data.write ops cw.data
+                >> Option.write ops ops.writeNumber4 cw.returnAddressIndex
+                >> ops.writeNumber4 cw.beginInputs
+                >> ops.writeNumber4 cw.beginOutputs
+                >> ops.writeNumber4 cw.inputsLength
+                >> ops.writeNumber4 cw.outputsLength
+                >> ops.writeNumber4 cw.cost
+        let read = reader {
+            let! discriminator = Byte.read
+            match discriminator with
+            | SerializedPKWitness ->
+                let! publicKey = readBytes Crypto.SerializedPublicKeyLength
+                let! signature = readBytes Crypto.SerializedSignatureLength
+                return PKWitness (publicKey, Signature signature)
+            | SerializedContractWitness ->
+                let! cHash = Hash.read
+                let! command = readString
+                let! data = Data.read
+                let! returnAddressIndex = Option.read readNumber4
+                let! beginInputs = readNumber4
+                let! beginOutputs = readNumber4
+                let! inputsLength = readNumber4
+                let! outputsLength = readNumber4
+                let! cost = readNumber4
+                return ContractWitness {
+                    cHash = Hash.Hash cHash
+                    command = command
+                    data = data
+                    returnAddressIndex = returnAddressIndex
+                    beginInputs = beginInputs
+                    beginOutputs = beginOutputs
+                    inputsLength = inputsLength
+                    outputsLength = outputsLength
+                    cost = cost
+                }
             | _ ->
                 yield! fail
         }
@@ -349,8 +536,8 @@ module private Serialization =
     module Block =
         let write ops bk =
             Header.write ops bk.header
-            >> List.write ops (fun ops hash -> ops.writeHash hash) ([ bk.txMerkleRoot; bk.witnessMerkleRoot; bk.activeContractSetMerkleRoot ] @ bk.commitments)
-            >> List.write ops (fun ops tx -> Transaction.write Full ops tx) bk.transactions
+            >> List.write ops (fun ops -> ops.writeHash) ([ bk.txMerkleRoot; bk.witnessMerkleRoot; bk.activeContractSetMerkleRoot ] @ bk.commitments)
+            >> List.write ops (fun ops -> Transaction.write Full ops) bk.transactions
         let read = reader {
             let! header = Header.read
             let! commitments = List.read Hash.read
@@ -416,3 +603,13 @@ module Block =
         
         Stream (bytes, 0)
         |> run readBk
+
+module Data =
+    let serialize data =
+        Data.write counters data 0
+        |> create 
+        |> Data.write serializers data
+        |> getBuffer
+    let deserialize bytes =
+        Stream (bytes, 0)
+        |> run Data.read
