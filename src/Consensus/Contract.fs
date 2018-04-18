1cea068ffae5f4554b9d853d7abbe489caf3b42d
A L Manning <asher@zenprotocol.com>
2018-04-18 16:41:55 +0300

    refactoring

--- src/Consensus/Contract.fs
+++ src/Consensus/Contract.fs
@@ -88,18 +88,18 @@ let private wrapMainFn (mainFn : zfstarMainFn) : ContractMainFn =
     fun txSkeleton (ContractId (version, Hash.Hash cHash)) command sender data contractWallet ->
         let txSkeleton' = ZFStar.fsToFstTxSkeleton txSkeleton
         let command' = ZFStar.fsToFstString command
-        let data' = ZFStar.fsToFstOption id data
+        let data' = ZFStar.fsToFstOption data
         let contractWallet' = ZFStar.convertWallet contractWallet
         mainFn txSkeleton' (version,cHash) command' sender data' contractWallet'
         |> ZFStar.unCost
         |> ZFStar.toResult
-        |> Result.bind ZFStar.convertResult
+        |> Result.map ZFStar.convertResult
 
 let private wrapCostFn (costFn: zfstarCostFn) : ContractCostFn =
     fun txSkeleton command sender data contractWallet ->
         let txSkeleton' = ZFStar.fsToFstTxSkeleton txSkeleton
         let command' = ZFStar.fsToFstString command
-        let data' = ZFStar.fsToFstOption id data
+        let data' = ZFStar.fsToFstOption data
         let contractWallet' = ZFStar.convertWallet contractWallet
         costFn txSkeleton' command' sender data' contractWallet'
         |> ZFStar.unCost
