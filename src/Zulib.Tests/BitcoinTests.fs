a59d420eff4612ae9ebc65d0c41e422090094ac4
Nathan Cook <nathan.cook@gmail.com>
2018-05-28 16:12:53 +0300

    Clean paket refs

---
+++ src/Zulib.Tests/BitcoinTests.fs
@@ -0,0 +1,10 @@
+﻿module Zulib.Tests.BitcoinTests
+
+open NUnit.Framework
+open System.Text
+open FsUnit
+open Infrastructure
+open FsCheck.NUnit
+open FStar.Pervasives.Native
+
+open Helper
---
+++ src/Zulib.Tests/Helper.fs
@@ -0,0 +1,5 @@
+﻿module Zulib.Tests.Helper
+
+module Cost = Zen.Cost.Realized
+
+let unCost (Cost.C inj:Zen.Cost.Realized.cost<'Aa, 'An>) : 'Aa = inj.Force()
