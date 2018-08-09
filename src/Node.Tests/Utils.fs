7f07f495743824b4dc3c78ed627a472cd76b92b0
somdoron <somdoron@gmail.com>
2018-08-09 09:55:14 +0300

    Fix Node.Tests actor shutdown order

--- src/Node.Tests/Utils.fs
+++ src/Node.Tests/Utils.fs
@@ -25,22 +25,22 @@ let createBroker () =
 
 let getActors () =
     [
-        createBroker ()        
-        Wallet.Main.main dataPath busName chain Wallet.Main.Wipe.NoWipe
-        Api.Main.main chain busName apiUri
+        createBroker ()
         Blockchain.Main.main dataPath chainParams busName false
         Network.Main.main dataPath busName chainParams "" false "" [] false false
+        Wallet.Main.main dataPath busName chain Wallet.Main.Wipe.NoWipe
+        Api.Main.main chain busName apiUri
     ]
-    |> List.map (fun x -> printfn ">>>%A" (x.GetHashCode()); x)
+    |> List.rev
     |> List.map Disposables.toDisposable
-    |> Disposables.fromList 
+    |> Disposables.fromList
 
 let split value seperators =
     (value:String).Split seperators
-    |> Array.choose (fun value -> 
-        if String.IsNullOrWhiteSpace value then 
-            None 
-        else 
+    |> Array.choose (fun value ->
+        if String.IsNullOrWhiteSpace value then
+            None
+        else
             Some value
     )
 
@@ -50,13 +50,13 @@ let private check (response:HttpResponse) =
     response.StatusCode
     |> should equal 200
     response
-    
+
 let private getBody (response:HttpResponse) =
     match response.Body with
     | Text string -> string
     | _ -> String.Empty
-        
-let get action = 
+
+let get action =
     action
     |> uri
     |> Http.Request
