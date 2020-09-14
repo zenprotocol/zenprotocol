fc19ed905e5e22d0f9a30652bdce65efe529200d
David S. Brima <david@brima.it>
2020-09-14 17:33:09 +0300

    feat: wallet can sync faster

--- src/Wallet/Account.fs
+++ src/Wallet/Account.fs
@@ -328,6 +328,10 @@ let addBlock dataAccess session blockHash block =
 
     {account with blockNumber = block.header.blockNumber; blockHash = blockHash}
     |> DataAccess.Account.put dataAccess session
+    eventX "Wallet adding block #{blockNumber} of block {blockHash}"
+    >> setField "blockNumber" block.header.blockNumber
+    >> setField "blockHash"   blockHash.AsString
+    |> Log.debug
 
 let undoBlock dataAccess session blockHash block =
     let account = DataAccess.Account.get dataAccess session
@@ -371,10 +375,51 @@ let undoBlock dataAccess session blockHash block =
 
     {account with blockNumber = block.header.blockNumber - 1ul; blockHash = block.header.parent}
     |> DataAccess.Account.put dataAccess session
+    eventX "Wallet remove block #{blockNumber} of block {blockHash}"
+    >> setField "blockNumber" block.header.blockNumber
+    >> setField "blockHash"   blockHash.AsString
+    |> Log.debug
 
-type private SyncAction =
-    | Undo of Block * Hash
-    | Add of Block * Hash
+type Action =
+    | UndoBlock
+    | AddBlock
+
+type SyncAction = Action * Block * Hash
+
+let fastSync dataAccess session tipBlockHash (tipHeader:BlockHeader) (headers: Map<Hash.Hash,Block>) =
+    let account = DataAccess.Account.get dataAccess session
+    // Find the fork block of the account and the blockchain, logging actions
+    // to perform. Undo each block in the account's chain but not the blockchain,
+    // and add each block in the blockchain but not the account's chain.
+    let getBlock hash = headers |> Map.find hash
+
+    let rec locate ((x,i),(y,j)) acc =
+
+        if x = y && i = j then acc
+        elif i > j
+        then
+            locate (((getBlock x).header.parent, i-1ul), (y,j))
+                ((AddBlock, getBlock x, x) :: acc)
+        elif i < j
+        then
+            locate ((x,i), ((getBlock y).header.parent, j-1ul))
+                ((UndoBlock, getBlock y, y) :: acc)
+        else
+            locate (((getBlock x).header.parent, i-1ul), ((getBlock y).header.parent, j-1ul))
+                ((AddBlock, getBlock x, x) :: (UndoBlock, getBlock y, y) :: acc)
+
+    let actions = locate ((tipBlockHash, tipHeader.blockNumber),
+                          (account.blockHash, account.blockNumber)) []
+
+    let toUndo, toAdd = List.partition (function | UndoBlock, _, _ -> true | _ -> false) actions
+    let toUndo = List.rev toUndo     // Blocks to be undo were found backwards.
+    let sortedActions = toUndo @ toAdd
+
+    sortedActions
+    |> List.iter (function
+        | UndoBlock, block,hash -> undoBlock dataAccess session hash block
+        | AddBlock, block,hash -> addBlock dataAccess session hash block
+        )
 
 let sync dataAccess session tipBlockHash (tipHeader:BlockHeader) (getHeader:Hash -> BlockHeader) (getBlock:Hash -> Block) =
     let account = DataAccess.Account.get dataAccess session
@@ -387,23 +432,24 @@ let sync dataAccess session tipBlockHash (tipHeader:BlockHeader) (getHeader:Hash
         if x = y && i = j then acc
         elif i > j
         then
-            locate (((getHeader x).parent, i-1ul), (y,j)) (Add (getBlock x, x) :: acc)
+            locate (((getHeader x).parent, i-1ul), (y,j)) ((AddBlock, getBlock x, x) :: acc)
         elif i < j
         then
-            locate ((x,i), ((getHeader y).parent, j-1ul)) (Undo (getBlock y, y) :: acc)
+            locate ((x,i), ((getHeader y).parent, j-1ul)) ((UndoBlock, getBlock y, y) :: acc)
         else
-            locate (((getHeader x).parent, i-1ul), ((getHeader y).parent, j-1ul)) (Add (getBlock x, x) :: Undo (getBlock y,y) :: acc)
+            locate (((getHeader x).parent, i-1ul), ((getHeader y).parent, j-1ul)) ((AddBlock, getBlock x, x) :: (UndoBlock, getBlock y,y) :: acc)
 
     let actions = locate ((tipBlockHash, tipHeader.blockNumber), (account.blockHash, account.blockNumber)) []
 
-    let toUndo, toAdd = List.partition (function | Undo _ -> true | _ -> false) actions
+    let toUndo, toAdd = List.partition (function | UndoBlock, _, _ -> true | _ -> false) actions
     let toUndo = List.rev toUndo     // Blocks to be undo were found backwards.
     let sortedActions = toUndo @ toAdd
 
     sortedActions
     |> List.iter (function
-        | Undo (block,hash) -> undoBlock dataAccess session hash block
-        | Add (block,hash) -> addBlock dataAccess session hash block)
+        | UndoBlock, block, hash -> undoBlock dataAccess session hash block
+        | AddBlock, block, hash -> addBlock dataAccess session hash block
+        )
 
 let delete dataAccess session =
     DataAccess.Outputs.truncate dataAccess session
--- src/Wallet/Main.fs
+++ src/Wallet/Main.fs
@@ -47,7 +47,16 @@ let eventHandler client event dataAccess session accountStatus =
 let private sync dataAccess session client =
     match Blockchain.getTip client with
     | Some (tipBlockHash,tipHeader) ->
-        Account.sync dataAccess session tipBlockHash tipHeader (Blockchain.getBlockHeader client >> Option.get) (Blockchain.getBlock client false >> Option.get)
+        let account = 
+            Account.tryGet dataAccess session
+            |> Option.map (fun x -> x.blockNumber)
+            |> Option.defaultValue 0ul
+        if tipHeader.blockNumber - account > 500ul then
+            Blockchain.getAllBlocks client
+            |> Map.map (fun _ b -> Serialization.Block.deserialize b |> Option.get) //this is sent over the messaging bus so we can be sure about the existences
+            |> Account.fastSync dataAccess session tipBlockHash tipHeader
+        else
+            Account.sync dataAccess session tipBlockHash tipHeader (Blockchain.getBlockHeader client >> Option.get) (Blockchain.getBlock client false >> Option.get)
 
         eventX "Account synced to block #{blockNumber} {blockHash}"
         >> setField "blockNumber" tipHeader.blockNumber
